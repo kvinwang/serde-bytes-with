2d063c5cffb107a8da70388df05b368ee5b60753
Kevin Wang <wy721@qq.com>
2024-04-18 02:19:55 +0000

    Accept other type of items as well

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ pub fn serde_bytes_with(attr: TokenStream, item: TokenStream) -> TokenStream {
     patch(parse_macro_input!(attr), parse_macro_input!(item)).into()
 }
 
-pub(crate) fn patch(serde_with: TokenStream2, item: ItemStruct) -> TokenStream2 {
+pub(crate) fn patch(serde_with: TokenStream2, item: TokenStream2) -> TokenStream2 {
     match patch_or_err(serde_with, item) {
         Ok(tokens) => tokens,
         Err(err) => err.to_compile_error(),
@@ -36,7 +36,10 @@ fn has_attr(attrs: &[syn::Attribute], name: &str, field: &str) -> bool {
     false
 }
 
-fn patch_or_err(serde_with: TokenStream2, mut input: ItemStruct) -> syn::Result<TokenStream2> {
+fn patch_or_err(serde_with: TokenStream2, input: TokenStream2) -> syn::Result<TokenStream2> {
+    let Ok(mut input) = syn::parse2::<ItemStruct>(input.clone()) else {
+        return Ok(input);
+    };
     for field in input.fields.iter_mut() {
         if has_attr(&field.attrs, "serde", "with") {
             continue;
