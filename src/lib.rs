6c203efda79c12ce4f5521975b32b9d837b29cb6
Kevin Wang <wy721@qq.com>
2024-04-18 03:10:09 +0000

    Compat for optional and repeated

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,12 @@ use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 
 #[proc_macro_attribute]
-pub fn serde_bytes_with(attr: TokenStream, item: TokenStream) -> TokenStream {
-    patch(parse_macro_input!(attr), parse_macro_input!(item)).into()
+pub fn serde_bytes_with(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    patch(parse_macro_input!(item)).into()
 }
 
-pub(crate) fn patch(serde_with: TokenStream2, item: TokenStream2) -> TokenStream2 {
-    match patch_or_err(serde_with, item) {
+pub(crate) fn patch(item: TokenStream2) -> TokenStream2 {
+    match patch_or_err(item) {
         Ok(tokens) => tokens,
         Err(err) => err.to_compile_error(),
     }
@@ -36,20 +36,30 @@ fn has_attr(attrs: &[syn::Attribute], name: &str, field: &str) -> bool {
     false
 }
 
-fn patch_or_err(serde_with: TokenStream2, input: TokenStream2) -> syn::Result<TokenStream2> {
+fn patch_or_err(input: TokenStream2) -> syn::Result<TokenStream2> {
     let Ok(mut input) = syn::parse2::<ItemStruct>(input.clone()) else {
         return Ok(input);
     };
     for field in input.fields.iter_mut() {
         if !has_attr(&field.attrs, "serde", "with") && has_attr(&field.attrs, "prost", "bytes") {
-            field
-                .attrs
-                .push(syn::parse_quote!(#[serde(with = #serde_with)]));
+            if has_attr(&field.attrs, "prost", "optional") {
+                field
+                    .attrs
+                    .push(syn::parse_quote!(#[serde(with = "::prpc::serde_helpers::option_bytes_as_hex_str")]));
+            } else if has_attr(&field.attrs, "prost", "repeated") {
+                field
+                    .attrs
+                    .push(syn::parse_quote!(#[serde(with = "::prpc::serde_helpers::vec_bytes_as_hex_str")]));
+            } else {
+                field.attrs.push(
+                    syn::parse_quote!(#[serde(with = "::prpc::serde_helpers::bytes_as_hex_str")]),
+                );
+            }
         }
-        if !has_attr(&field.attrs, "serde", "default") && has_attr(&field.attrs, "prost", "repeated") {
-            field
-                .attrs
-                .push(syn::parse_quote!(#[serde(default)]));
+        if !has_attr(&field.attrs, "serde", "default")
+            && has_attr(&field.attrs, "prost", "repeated")
+        {
+            field.attrs.push(syn::parse_quote!(#[serde(default)]));
         }
     }
 
--- src/tests.rs
+++ src/tests.rs
@@ -1,23 +1,22 @@
 #[test]
 fn test_expand() {
-    let stream = crate::patch(
-        syn::parse_quote!("as_hexed_bytes"),
-        syn::parse_quote! {
-            #[derive(Serialize, Deserialize, TypeInfo)]
-            #[derive(Clone, PartialEq, Message)]
-            pub struct Attestation {
-                #[prost(int32, tag = "1")]
-                pub version: i32,
-                #[prost(string, tag = "2")]
-                pub provider: ::prost::alloc::string::String,
-                #[prost(message, optional, tag = "3")]
-                pub payload: ::core::option::Option<AttestationReport>,
-                #[prost(bytes = "vec", tag = "5")]
-                pub encoded_report: ::prost::alloc::vec::Vec<u8>,
-                #[prost(bytes = "vec", tag = "6")]
-                pub timestamp: ::prost::alloc::vec::Vec<u8>,
-            }
-        },
-    );
+    let stream = crate::patch(syn::parse_quote! {
+        #[derive(Serialize, Deserialize, TypeInfo)]
+        #[derive(Clone, PartialEq, Message)]
+        pub struct Attestation {
+            #[prost(int32, tag = "1")]
+            pub version: i32,
+            #[prost(string, tag = "2")]
+            pub provider: String,
+            #[prost(message, optional, tag = "3")]
+            pub payload: Option<AttestationReport>,
+            #[prost(bytes = "vec", tag = "5")]
+            pub encoded_report: Vec<u8>,
+            #[prost(bytes = "vec", optional, tag = "6")]
+            pub timestamp: Option<Vec<u8>>,
+            #[prost(bytes = "vec", repeated, tag = "7")]
+            pub timestamp: Vec<Vec<u8>>,
+        }
+    });
     insta::assert_snapshot!(rustfmt_snippet::rustfmt_token_stream(&stream).unwrap())
 }
